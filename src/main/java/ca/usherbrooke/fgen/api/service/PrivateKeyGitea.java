7a43ec9ed8440e160ed28ddcc346beba4fcb748c
Guillaume Dumas <94912626+Jio025@users.noreply.github.com>
2025-09-25 09:50:57 -0400

    Delete src/main/java/ca/usherbrooke/fgen/api/service/PrivateKeyGitea.java

--- src/main/java/ca/usherbrooke/fgen/api/service/PrivateKeyGitea.java
+++
@@ -1,9 +0,0 @@
-package ca.usherbrooke.fgen.api.service;
-
-public class PrivateKeyGitea {
-    final static private String token = "5b86fb45c5392275b8e7c4f8199e7f8969a3119e";
-    // Getter function
-    public String getToken(){
-        return token;
-    }
-}
